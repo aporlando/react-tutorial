eabde81b518807a4cba41691539ffbe6ac6d1b1c
aporlando <alison.orlando@gobigriver.com>
2015-08-19 22:27:11 -0400

    add comments to the express server and serve up the html file

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ var fs = require("fs"), json;
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+//This allows CORS (Cross Origin Resource Sharing) for reading and writing to comments.json
+//As explained at http://jonathanmh.com/how-to-enable-cors-in-express-js-node-js/
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+//read from a file
 function readJsonFileSync(filepath, encoding){
 
     if (typeof (encoding) == 'undefined'){
@@ -20,6 +23,7 @@ function readJsonFileSync(filepath, encoding){
     return JSON.parse(file);
 }
 
+//write content to a file
 function writeJsonFileSync(filepath, content){
 
     if (typeof (encoding) == 'undefined'){
@@ -29,17 +33,19 @@ function writeJsonFileSync(filepath, content){
     return;
 }
 
+//serve the react tutorial's index.html file
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile('./index.html');
 });
 
+//send the comments as json
 app.get('/comments', function (req, res) {
   res.send(readJsonFileSync("./comments.json"));
 });
 
+//write comments to json file
 app.post('/comments', function (req, res) {
   var previousComments = readJsonFileSync("./comments.json");
-  console.log(req.body);
   previousComments.push({author: req.body.author, text: req.body.text });
   writeJsonFileSync("./comments.json", JSON.stringify(previousComments, null, 4));
   res.send(previousComments);
